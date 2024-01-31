b2ce194b24e733c5e70c6dc84d52f94f382e9abd
dilmer <dilmer.salvador2015@gmail.com>
2024-01-31 13:19:32 -0500

    added some solution dor the current task to dilmer classes

--- src/week01/question01/dilmer.java
+++ src/week01/question01/dilmer.java
@@ -7,4 +7,26 @@ public class dilmer {
 
 
     }
+
+    public static boolean isEvenNumber(int num){
+        return num%2==0;
+    }
+
+    public static String isEvenOrOdd(int num){
+        if(num % 2 == 0 ){
+            return "Even";
+        }else {
+            return "Odd";
+        }
+    }
+
+    public static void oddOrEven(int number){
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is even number");
+        } else {
+            System.out.println(number + " is odd number");
+        }
+
+    }
 }
--- src/week01/question02/dilmer.java
+++ src/week01/question02/dilmer.java
@@ -6,4 +6,44 @@ public class dilmer {
 
 
     }
+
+    //divide
+    public static int simpleDivide(int a, int b) {
+
+        int count = 0;
+        while (a >= b) {
+            a -= b;
+            count++;
+        }
+
+        return count;
+    }
+
+    // found this answer in the internet
+    public static int binaryDivide(int dividend, int divisor) {
+        int current = 1;
+        int denom = divisor;
+        // This step is required to find the biggest current number which can be
+        // divided with the number safely.
+        while (denom <= dividend) {
+            current <<= 1;
+            denom <<= 1;
+        }
+        // Since we may have increased the denominator more than dividend
+        // then we need to go back one shift, and same would apply for current.
+        denom >>= 1;
+        current >>= 1;
+        int answer = 0;
+        // Now deal with the smaller number.
+        while (current != 0) {
+            if (dividend >= denom) {
+                dividend -= denom;
+                answer |= current;
+            }
+            current >>= 1;
+            denom >>= 1;
+        }
+        return answer;
+    }
+
 }
