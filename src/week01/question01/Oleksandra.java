1a61bd8d951f9febb6ef0d613518e67a9e0799a5
oleksandrafernandez <sashafernandez85@me.com>
2024-01-31 18:28:22 -0500

    Added to Q1 Method 1, Method 2 and Method 3.

--- src/week01/question01/Oleksandra.java
+++ src/week01/question01/Oleksandra.java
@@ -2,6 +2,32 @@ package week01.question01;
 
 public class Oleksandra {
     public static void main(String[] args) {
+        OddOrEven(50);
+        System.out.println(isEvenNum(60));
+        System.out.println(isEven(50));
 
     }
+    //Method 1
+public static void OddOrEven(int num){
+
+
+        if(num % 2 == 0){
+            System.out.println(num + " is even.");
+        }else {
+            System.out.println(num + " is odd.");
+        }
 }
+// Method 2
+    public static boolean isEvenNum(int num){
+        return num % 2 == 0;
+    }
+ // Method 3
+    public static boolean isEven(int num){
+        if(num % 2 == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
