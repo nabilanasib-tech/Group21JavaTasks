0ce62ab6b7b55d565430332e7dde5727d4eded19
BatdorjJava <tabyartooj@hotmail.com>
2024-02-07 14:50:18 -0500

    - QA-2 solution1

---
+++ src/week02/question2/Batdorj.java
@@ -0,0 +1,20 @@
+package week02.question2;
+
+public class Batdorj {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.println (i + " is divisible by 15" );
+            }
+            if (i % 5 == 0) {
+                System.out.println (i + " is divisible by 5" );
+            }
+            if (i % 3 == 0) {
+                System.out.println(i + " is divisible by 3");
+            }
+        }
+
+    }
+}
