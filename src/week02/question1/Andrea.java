50561af0a749de354e2c387fa588af306eeb2ff2
Andrea Gutierrez <drea.guti@gmail.com>
2024-02-07 15:43:52 -0500

    added Andrea classes - week 02

---
+++ src/week02/question1/Andrea.java
@@ -0,0 +1,7 @@
+package week02.question1;
+
+public class Andrea {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/week02/question3/Andrea.java
@@ -0,0 +1,7 @@
+package week02.question3;
+
+public class Andrea {
+    public static void main(String[] args) {
+
+    }
+}
