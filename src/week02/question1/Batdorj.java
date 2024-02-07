9b5d5e880102fb14fb35aec38de6c2f8a6f21a82
BatdorjJava <tabyartooj@hotmail.com>
2024-02-07 14:26:36 -0500

    week2 QA-1 commited

---
+++ src/week02/question1/Batdorj.java
@@ -0,0 +1,17 @@
+package week02.question1;
+
+public class Batdorj {
+
+    public static void main(String[] args) {
+
+        int x = 4,
+                y = 10;
+
+        x = x + y;
+        y = x - y;
+        x = x - y;
+
+        System.out.println ("X = "+ x );
+        System.out.println ("Y = "+ y );
+    }
+}
