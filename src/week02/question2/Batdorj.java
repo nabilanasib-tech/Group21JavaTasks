bdf7265d625d213916cc1248ccfb36b1b480046c
BatdorjJava <tabyartooj@hotmail.com>
2024-02-07 15:12:50 -0500

    - QA-2 added solution2

--- src/week02/question2/Batdorj.java
+++ src/week02/question2/Batdorj.java
@@ -16,5 +16,27 @@ public class Batdorj {
             }
         }
 
+        // solution #2
+        System.out.print("Divisible by 15 = ");
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.print(i+", ");
+            }
+        }
+        System.out.println ( );
+        System.out.print("Divisible by 5 = ");
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0 && i % 15 != 0) {
+                System.out.print(i+", ");
+            }
+        }
+        System.out.println ( );
+        System.out.print("Divisible by 3 = ");
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 15 != 0) {
+                System.out.print(i+ ", ");
+            }
+        }
+        System.out.println ( );
     }
 }
