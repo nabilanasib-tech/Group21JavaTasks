6a895d4d9b896b2e3857f0a431a465411eb08dae
dreaguti <drea.guti@gmail.com>
2024-02-14 12:18:26 -0500

    added solution for question 2 - Andrea class

--- src/week02/question2/Andrea.java
+++ src/week02/question2/Andrea.java
@@ -2,10 +2,28 @@ package week02.question2;
 
 public class Andrea {
     public static void main(String[] args) {
+        int n = 30;
 
+        String by15 = "";
+        String by5 = "";
+        String by3 = "";
+
+        for (int i = 1; i <= n; i++) {
+
+            if (i % 15 == 0) {
+                by15 += i + " ";
+            } else if (i % 5 == 0) {q
+                by5 += i + " ";
+            } else if (i % 3 == 0) {
+                by3 += i + " ";
+            }
+        }
+
+        System.out.println("Divisible by 15: " + by15);
+        System.out.println("Divisible by 5: " + by5);
+        System.out.println("Divisible by 3: " + by3);
     }
 }
-
 
 /*
     Numbers -- Divisible by 3, 5, 15
