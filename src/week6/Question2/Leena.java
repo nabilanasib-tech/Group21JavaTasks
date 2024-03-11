b1740311f027692e5b4bf2b6538c653dcd1a0e7a
leena <leena@cybertekschool.com>
2024-03-11 21:20:56 +0300

    added week 6 tasks

---
+++ src/week6/Question2/Leena.java
@@ -0,0 +1,12 @@
+package week6.Question2;
+
+import java.util.Arrays;
+
+public class Leena {
+    /*
+    Find Maximum:
+  Write a method that can find the maximum number from an int Array.
+     */
+
+
+}
