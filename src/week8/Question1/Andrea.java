2c039cef55d3d3c4e913c7c62c4869828ebece29
dreaguti <drea.guti@gmail.com>
2024-04-02 14:25:46 -0400

    added week 8 java tasks

---
+++ src/week8/Question1/Andrea.java
@@ -0,0 +1,12 @@
+package week8.Question1;
+
+/*
+ -- Array - Move Zeroes to the end --
+
+        Write a method that can move all the zeros to last indexes of the array (Do Not Use Sort Method)
+            input:  {1,0,2,0,3,0,4,0};
+            output: [1, 2, 3, 4, 0, 0, 0, 0]
+
+ */
+public class Andrea {
+}
