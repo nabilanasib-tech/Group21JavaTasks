428a1cdff3d46e1549509424a48015aeaf360fe6
dreaguti <drea.guti@gmail.com>
2024-04-03 11:24:15 -0400

    added week 8 java task solutions - Andrea

--- src/week8/Question1/Andrea.java
+++ src/week8/Question1/Andrea.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
  -- Array - Move Zeroes to the end --
 
         Write a method that can move all the zeros to last indexes of the array (Do Not Use Sort Method)
-            input:  {1,0,2,0,3,0,4,0};
+            input:  {1, 0, 2, 0, 3, 0, 4, 0};
             output: [1, 2, 3, 4, 0, 0, 0, 0]
 
  */
--- src/week8/Question2/Andrea.java
+++ src/week8/Question2/Andrea.java
@@ -3,8 +3,8 @@ package week8.Question2;
 /*
  -- Array - n Unique Integers That Sum Up To 0 --
 
-        Write a function that, given an integer N (1 < N < 100), returns an array containing N unique integers that sum up to 0.
-            The function can return any such array.
+                Write a function that, given an integer N (1 < N < 100), returns an array containing N unique integers that sum up to 0.
+                The function can return any such array.
                 For example, given N = 4, the function could return [1, 0, -3, 2] or [-2, 1, -4, 5].
                 The answer [1, -1, 1, 3] would be incorrect (because value 1 occurs twice).
                 For N = 3 one of the possible answers is [-1,0,1]  (but there are many more correct answers).
@@ -19,9 +19,9 @@ public class Andrea {
 
     public static int[] sumZero(int n) {
         int[] answer = new int[n];
-        for (int i = 1; i<n; i+=2) {
-            answer[i-1]=i;
-            answer[i]=-i;
+        for (int i = 1; i < n; i += 2) {
+            answer[i - 1] = i;
+            answer[i] = -i;
         }
         return answer;
     }
