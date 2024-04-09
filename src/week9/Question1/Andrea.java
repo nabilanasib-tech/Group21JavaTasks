95987d8ffcd56574af447d30e4086c024d19e5a8
dreaguti <drea.guti@gmail.com>
2024-04-09 15:13:03 -0400

    added week 9 java solutions - Andrea

--- src/week9/Question1/Andrea.java
+++ src/week9/Question1/Andrea.java
@@ -3,11 +3,10 @@ package week9.Question1;
 import java.util.Arrays;
 
 public class Andrea {
-
-    /*
+/*
     1 - Array_concat2Arrays -
     Write a return method that can concatenate two arrays
-     */
+*/
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 3};
         int[] arr2 = {4, 5, 6};
@@ -32,3 +31,12 @@ public class Andrea {
         return result;
     }
 }
+
+
+
+/*
+In the above program, instead of using arraycopy, we manually copy each element of both arrays array1 and array2 to result.
+We store the total length required for result, i.e. array1.length + array2. length. Then, we create a new array result of the length.
+Now, we use the for-each loop to iterate through each element of array1 and store it in the result. After assigning it, we increase the position pos by 1, pos++.
+Likewise, we do the same for array2 and store each element in result starting from the position after array1.
+ */
--- src/week9/Question2/Andrea.java
+++ src/week9/Question2/Andrea.java
@@ -2,22 +2,20 @@ package week9.Question2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class Andrea {
-/*
-    2 - List_RemoveNameAhmed -
-        Given a list of people names: "Ahmed", "John", Eric", "Ahmed", "Aaron".....
-        Write a java operation to remove all the names named Ahmed
- */
+    /*
+        2 - List_RemoveNameAhmed -
+            Given a list of people names: "Ahmed", "John", Eric", "Ahmed", "Aaron".....
+            Write a java operation to remove all the names named Ahmed
+     */
     public static void main(String[] args) {
         String[] names = {"Ahmed", "John", "Eric", "Ahmed", "Aaron"};
-        List<String> list =  new ArrayList<String>();
-        Collections.addAll(list, names);
-        list.remove("Ahmed");
-        list.remove("Ahmed");
-        names = list.toArray(new String[list.size()]);
-        System.out.println(Arrays.toString(names));
+        System.out.println("Original: " + Arrays.toString(names));
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(names));
+        for (int i = 0; i < names.length; i++) {
+            list.remove("Ahmed");
+        }
+        System.out.println("New: " + list);
     }
 }
